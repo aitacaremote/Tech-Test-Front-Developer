e7a78bb060f6692fe779a7665cc0c0690412b4fe
Joaquin Velazquez <joacocoder@gmail.com>
2023-10-13 11:43:22 -0300

    chore: Card service reformulated

--- src/app/services/card.service.ts
+++ src/app/services/card.service.ts
@@ -22,35 +22,47 @@ export class CardService {
     return this.cards.value.find( card => card.id == id)
   }
 
-  deleteCard(id: string){
-    let newCards = this.cards.value
-    console.log(this.cards.value.findIndex( (card: Card) => card.id == id))
-    newCards.splice(this.cards.value.findIndex( (card: Card) => card.id == id), 1)
-    this.cards.next(newCards)
-    localStorage.setItem('card', this.cards.value.map( card => JSON.stringify(card)).join('&-&'))
-    this._snackBar.open('Card deleted successfully', 'ready', {
-      duration: 2000
-    })
+  deleteCard(id: string) {
+    const cards = this.cards.value;
+    const cardIndex = cards.findIndex(card => card.id === id);
+  
+    if (cardIndex !== -1) {
+      cards.splice(cardIndex, 1);
+      this.cards.next(cards);
+      this.updateLocalStorage();
+      this.showSnackBar('Card deleted successfully');
+    }
   }
-
-  createCard(card: Card){
-    let newCards = this.cards.value
-    newCards.push(card)
-    this.cards.next(newCards)
-    localStorage.setItem('card', this.cards.value.map( card => JSON.stringify(card)).join('&-&'))
-    this._snackBar.open('Card created successfully', 'ready', {
-      duration: 2000
-    })
+  
+  createCard(card: Card) {
+    const cards = this.cards.value;
+    cards.push(card);
+    this.cards.next(cards);
+    this.updateLocalStorage();
+    this.showSnackBar('Card created successfully');
   }
-
-  updateCard(cardUpdating: Card){
-    let newCards = this.cards.value
-    newCards[newCards.findIndex( card => card.id == cardUpdating.id)] = cardUpdating
-    this.cards.next(newCards)
-    localStorage.setItem('card', this.cards.value.map( card => JSON.stringify(card)).join('&-&'))
-    this._snackBar.open('Card Updated successfully', 'ready', {
+  
+  updateCard(cardUpdating: Card) {
+    const cards = this.cards.value;
+    const cardIndex = cards.findIndex(card => card.id === cardUpdating.id);
+  
+    if (cardIndex !== -1) {
+      cards[cardIndex] = cardUpdating;
+      this.cards.next(cards);
+      this.updateLocalStorage();
+      this.showSnackBar('Card updated successfully');
+    }
+  }
+  
+  updateLocalStorage() {
+    const serializedCards = this.cards.value.map(card => JSON.stringify(card)).join('&-&');
+    localStorage.setItem('card', serializedCards);
+  }
+  
+  showSnackBar(message: string) {
+    this._snackBar.open(message, 'ready', {
       duration: 2000
-    })
+    });
   }
 
 }
