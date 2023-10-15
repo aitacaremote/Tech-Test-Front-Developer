f04bb65710de257d263bde3764c2afb5890e3820
6emre <emre.derman@ug.bilkent.edu.tr>
2023-10-15 18:15:09 +0300

    unused component removed

--- aitaca-application/src/app/app.module.ts
+++ aitaca-application/src/app/app.module.ts
@@ -3,14 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ApplicationPageFormComponent } from './application-page-form/application-page-form.component';
-import { ApplicationPageSubmitButtonComponent } from './application-page-submit-button/application-page-submit-button.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';  
 
 @NgModule({
   declarations: [
     AppComponent,
-    ApplicationPageFormComponent,
-    ApplicationPageSubmitButtonComponent
+    ApplicationPageFormComponent
   ],
   imports: [
     BrowserModule,
