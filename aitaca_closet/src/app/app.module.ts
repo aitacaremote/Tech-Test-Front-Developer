733f1269e2b68288675c0000b0edfa3e5b5052d1
SandraRial <sandrarialperez@gmail.com>
2023-10-15 12:58:20 +0200

    form structure

--- aitaca_closet/src/app/app.module.ts
+++ aitaca_closet/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,9 @@ import { ContactComponent } from './contact/contact.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- aitaca_closet/src/app/contact/contact.component.ts
+++ aitaca_closet/src/app/contact/contact.component.ts
@@ -1,10 +1,36 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.scss']
+  styleUrls: ['./contact.component.scss'],
 })
 export class ContactComponent {
 
+  contactForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.contactForm = this.fb.group({
+      nombre: ['', Validators.required],
+      telefono: ['', [Validators.required, Validators.minLength(9)]],
+      email: ['', [Validators.required, Validators.email]],
+      mensaje: ['', Validators.required],
+    });
+  }
+
+  submitForm() {
+    alert('enviandose el mensaje');
+    if (this.contactForm.valid) {
+      alert('Mensaje enviado')
+      console.log('Formulario enviado:', this.contactForm.value);
+      this.contactForm.reset();
+    } else {
+      alert('Debe rellenar todos los campos antes de enviar')
+      console.log(
+        'El formulario no es válido. Por favor, complete todos los campos correctamente.'
+      );
+    }
+  }
 }
